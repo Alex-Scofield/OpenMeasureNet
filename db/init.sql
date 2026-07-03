56c7bd43eebfac5ad97ad605c04bbd3e47d047d9
Alex Scofield <alexscofieldt@gmail.com>
2026-07-03 17:07:57 +0200

    Add databse initialisation

--- db/init.sql
+++ db/init.sql
@@ -1,15 +1,32 @@
--- Create a "users" table
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
+CREATE EXTENSION IF NOT EXISTS postgis; 
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
     email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
- 
--- Insert sample data
-INSERT INTO users (username, email) VALUES
-('alice', 'alice@example.com'),
-('bob', 'bob@example.com');
- 
--- Verify the data (optional, for debugging)
-SELECT * FROM users;
+
+CREATE TABLE versions (
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE boitiers (
+    id UUID PRIMARY KEY,
+    user_id UUID REFERENCES users(id),
+    version_id SERIAL REFERENCES versions(id)
+);
+
+CREATE TABLE quantities (
+    id SERIAL PRIMARY KEY,
+    unit VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE observations (
+    time TIMESTAMP NOT NULL,
+    boitier_id UUID REFERENCES boitiers(id),
+    quantity SERIAL REFERENCES quantities(id),
+    value REAL NOT NULL,
+    location GEOMETRY(POINT, 4326),
+    PRIMARY KEY (time, boitier_id, value)
+);
